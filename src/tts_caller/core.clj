1d2f6dd5e856418e175af3e70f3f1feeae3d9181
Yuv <ytrubetskoy@gmail.com>
2025-06-18 17:59:48 +0400

    core fix 23 juffff

--- src/tts_caller/core.clj
+++ src/tts_caller/core.clj
@@ -17,27 +17,25 @@
 (def config-path (str baresip-home "/config"))
 
 (defn ensure-baresip-config []
-  (let [accounts-file (File. accounts-path)
-        config-file (File. config-path)]
-    (.mkdirs (File. baresip-home))
-    ;; accounts
-    (when-not (.exists accounts-file)
-      (spit accounts-path
-            (str "sip:" sip-user "@" sip-domain ":5060"
-                 ";auth_user=" sip-user
-                 ";auth_pass=" sip-pass
-                 ";transport=udp;regint=0\n")))
-    ;; config
-    (spit config-path
-          (str "module_path /usr/lib64/baresip/modules\n"
-               "module g711.so\n"
-               "module aufile.so\n"
-               "module cons.so\n\n"
-               "sip_transp udp\n"
-               "sip_listen 0.0.0.0\n"
-               "audio_player aufile\n"
-               "audio_source aufile\n"
-               "audio_path /tmp/final.wav\n"))))
+  (.mkdirs (File. baresip-home))
+  ;; Always rewrite accounts
+  (spit accounts-path
+        (str "sip:" sip-user "@" sip-domain ":5060"
+             ";auth_user=" sip-user
+             ";auth_pass=" sip-pass
+             ";transport=udp;regint=0\n"))
+  ;; Always rewrite config
+  (spit config-path
+        (str "module_path /usr/lib64/baresip/modules\n"
+             "module g711.so\n"
+             "module aufile.so\n"
+             "module cons.so\n\n"
+             "sip_transp udp\n"
+             "sip_listen 0.0.0.0\n"
+             "audio_player aufile\n"
+             "audio_source aufile\n"
+             "audio_path /tmp/final.wav\n")))
+
 
 (defn call-sip [final-wav phone]
   (ensure-baresip-config)
