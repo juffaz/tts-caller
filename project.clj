fba69f21f4ec912cb3d0102eb68f1c526da75177
Juffaz <38725561+juffaz@users.noreply.github.com>
2025-06-24 10:21:30 +0400

    Update project.clj -add jar lib - delete dev profile

--- project.clj
+++ project.clj
@@ -7,12 +7,10 @@
                  [compojure "1.7.1"]
                  [clout "2.2.1"]
                  [instaparse "1.4.8"]
-                 [medley "1.4.0"]
-                 ]
-  :main tts-caller.core                      ;; <- used during uberjar
-  :uberjar-name "tts-caller-standalone.jar"           ;; <--- added
-  :aot [tts-caller.core]                     ;; <- Required for .jar
+                 [medley "1.4.0"]]
+  :main tts-caller.core
+  :uberjar-name "tts-caller-standalone.jar"
+  :aot [tts-caller.core]
   :resource-paths ["resources"]
   :extra-classpath-dirs ["lib"]
-  :profiles {:dev {:jvm-opts ["-Dmary.base=lib"]}} ;; <- Important for Calva + lein repl
-  :repositories [["central" "https://repo1.maven.org/maven2/"]])
+  :jvm-opts ["-Dmary.base=lib"])
