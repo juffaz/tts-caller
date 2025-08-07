7ab2d5d8f69526e6cbc6d9f9b694afa0254ffbb4
Juffaz <38725561+juffaz@users.noreply.github.com>
2025-08-07 16:14:59 +0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,15 +5,13 @@
                  [ring/ring-core "1.11.0"]
                  [ring/ring-jetty-adapter "1.11.0"]
                  [compojure "1.7.1"]
-                 [clout "2.2.1"]
                  [instaparse "1.4.8"]
                  [medley "1.4.0"]
-                 [org.slf4j/slf4j-simple "1.7.36"]
-                 ]
+                 [org.slf4j/slf4j-simple "1.7.36"]]
   :main tts-caller.core
   :uberjar-name "tts-caller-standalone.jar"
   :aot [tts-caller.core]
   :resource-paths ["resources"]
-  :jvm-opts ["-Dmary.base=lib"] ;; Apply mary.base globally
+  :jvm-opts ["-Dmary.base=lib"]
   :profiles {:dev {:jvm-opts ["-Dmary.base=lib"]}}
   :repositories [["central" "https://repo1.maven.org/maven2/"]])
